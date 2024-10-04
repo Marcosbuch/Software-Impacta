976377e624482968321e562dfa40d31a6466cb62
Marcosbuch <marcosbuch@gmail.com>
2024-10-04 16:34:09 -0300

    adicionar templates e app.py

--- app.py
+++ app.py
@@ -4,45 +4,102 @@ from datetime import datetime
 
 app = Flask(__name__)
 
-# Conexão com o banco de dados
-db = mysql.connector.connect(
-    host="127.0.0.1",
-    user="root",
-    password="Buch_159753",  # Insira a senha do MySQL
-    database="assessoria_financeira"
-)
-cursor = db.cursor()
+# Configuração da conexão com o banco de dados
+def get_db_connection():
+    return mysql.connector.connect(
+        host="127.0.0.1",
+        user="root",
+        password="Buch_159753",  # Insira a senha do MySQL
+        database="assessoria_financeira"
+    )
 
-# Rota para renderizar a página principal
+# Página inicial
 @app.route('/')
 def home():
-    return render_template('home.html')  # Página principal de navegação
+    return render_template('home.html')
 
-# Rota para renderizar a página de cadastro de receitas
+
+# Receita
+
+# Página de cadastro de receita
 @app.route('/cadastrar-receita', methods=['GET'])
 def render_cadastrar_receita():
     return render_template('cadastrar_receita.html')
 
-# Rota para cadastrar receita no banco de dados
+# Rota para cadastrar receita
 @app.route('/cadastrar-receita', methods=['POST'])
 def cadastrar_receita():
     data = request.json
-    descricao = data.get('descricao')
-    tipo = data.get('tipo')
-    indicador1 = data.get('indicador1')
-    indicador2 = data.get('indicador2')
-    compoe_dre = data.get('compoe_dre')
-    compoe_dfc = data.get('compoe_dfc')
+    descricao_receita = data.get('descricao_receita')
+    tipo_receita = data.get('tipo_receita')
+    indicador1_receita = data.get('indicador1_receita')
+    indicador2_receita = data.get('indicador2_receita')
+    compoe_dre_receita = data.get('compoe_dre_receita')
+    compoe_dfc_receita = data.get('compoe_dfc_receita')
 
-    # Adiciona a data atual
-    data_atual = datetime.now().strftime('%Y-%m-%d')
+    connection = get_db_connection()
+    cursor = connection.cursor()
 
-    sql = "INSERT INTO plano_contas_receitas (descricao, tipo, indicador1, indicador2, compoe_dre, compoe_dfc, data) VALUES (%s, %s, %s, %s, %s, %s, %s)"
-    values = (descricao, tipo, indicador1, indicador2, compoe_dre, compoe_dfc, data_atual)
-    cursor.execute(sql, values)
-    db.commit()
+    try:
+        sql = """
+        INSERT INTO plano_contas_receitas 
+        (descricao_receita, tipo_receita, indicador1_receita, indicador2_receita, compoe_dre_receita, compoe_dfc_receita, data_receita) 
+        VALUES (%s, %s, %s, %s, %s, %s, %s)
+        """
+        values = (descricao_receita, tipo_receita, indicador1_receita, indicador2_receita, compoe_dre_receita, compoe_dfc_receita, datetime.now())
+        cursor.execute(sql, values)
+        connection.commit()
+        message = 'Receita cadastrada com sucesso!'
+    except Exception as e:
+        connection.rollback()
+        message = f'Ocorreu um erro: {str(e)}'
+    finally:
+        cursor.close()
+        connection.close()
 
-    return jsonify({'message': 'Receita cadastrada com sucesso!'})
+    return jsonify({'message': message})
+
+
+# Despesa
+
+# Página de cadastro de despesa
+@app.route('/cadastrar-despesa', methods=['GET'])
+def render_cadastrar_despesa():
+    return render_template('cadastrar_despesa.html')
+
+# Rota para cadastrar despesa
+@app.route('/cadastrar-despesa', methods=['POST'])
+def cadastrar_despesa():
+    data = request.json
+    descricao_despesa = data.get('descricao_despesa')
+    tipo_despesa = data.get('tipo_despesa')
+    indicador1_despesa = data.get('indicador1_despesa')
+    indicador2_despesa = data.get('indicador2_despesa')
+    compoe_dre_despesa = data.get('compoe_dre_despesa')
+    compoe_dfc_despesa = data.get('compoe_dfc_despesa')
+
+    connection = get_db_connection()
+    cursor = connection.cursor()
+
+    try:
+        sql = """
+        INSERT INTO plano_contas_despesas 
+        (descricao_despesa, tipo_despesa, indicador1_despesa, indicador2_despesa, compoe_dre_despesa, compoe_dfc_despesa, data_despesa) 
+        VALUES (%s, %s, %s, %s, %s, %s, %s)
+        """
+        values = (descricao_despesa, tipo_despesa, indicador1_despesa, indicador2_despesa, compoe_dre_despesa, compoe_dfc_despesa, datetime.now())
+        cursor.execute(sql, values)
+        connection.commit()
+        message = 'Despesa cadastrada com sucesso!'
+    except Exception as e:
+        connection.rollback()
+        message = f'Ocorreu um erro: {str(e)}'
+    finally:
+        cursor.close()
+        connection.close()
+
+    return jsonify({'message': message})
+
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
