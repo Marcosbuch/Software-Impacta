46dcbfe34ff338707ebea166edacd74fd0c08f7d
Marcosbuch <marcosbuch@gmail.com>
2024-10-28 10:48:53 -0300

    adicionar templates e app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def home():
     return render_template('home.html')
 
 
-# Receita
+# Cadastramento Receita
 
 # Página de cadastro de receita
 @app.route('/cadastrar-receita', methods=['GET'])
@@ -60,7 +60,7 @@ def cadastrar_receita():
     return jsonify({'message': message})
 
 
-# Despesa
+# Cadastramento Despesa
 
 # Página de cadastro de despesa
 @app.route('/cadastrar-despesa', methods=['GET'])
@@ -101,5 +101,46 @@ def cadastrar_despesa():
     return jsonify({'message': message})
 
 
+
+# Cadastramento Bancos
+
+@app.route('/cadastrar-caixa', methods=['GET'])
+def render_cadastrar_caixa():
+    return render_template('cadastrar_caixa.html')
+
+#Rota para cadastrar Caixas
+@app.route('/cadastrar-caixa', methods=['POST'])
+def cadastrar_caixa():
+    data = request.json
+    nome_banco = data.get('nome_banco')
+    bandeira = data.get('bandeira')
+    tipo_de_pagamento =  data.get('tipo_de_pagamento')
+    taxa =  data.get('taxa')
+    prazo =  data.get('prazo')
+
+    connection = get_db_connection()
+    cursor = connection.cursor()
+
+    try:
+        sql = """
+        INSERT INTO caixa
+        (nome_banco, bandeira, tipo_de_pagamento, taxa, prazo, data_caixa)
+        VALUES(%s,%s,%s,%s,%s,%s)
+        """
+
+        values = (nome_banco, bandeira, tipo_de_pagamento, taxa, prazo, datetime.now())
+        cursor.execute(sql, values)
+        connection.commit()
+        message= 'Caixa cadastrado com sucesso'
+    except Exception as e:
+        connection.rollback()
+        message = f'Ocorreu um erro: {str(e)}'
+    finally:
+        cursor.close()
+        connection.close()
+
+    return jsonify({'message': message})
+
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
+
